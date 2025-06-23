62523cbb840427f935a06646ea800bac1876636c
Juanje1997 <juanjesusdp1@outlook.es>
2025-06-23 17:51:01 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+    
+let dominios=[".net", ".com", ".info", ".biz", ".org", ".edu"];
+  
+const domain= pronouns.flatMap(pronoun=> adjs.flatMap(adj=>nouns.flatMap(noun=> dominios.map(domi=>`${pronoun}${adj}${noun}${domi}`))));
+
+
+      
+const buttons = document.querySelectorAll('[data-choice]');
+
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		const domainChoice = button.getAttribute('data-choice');
+		const result = domain.filter(element=> element.endsWith(domainChoice));
+    console.log(`Dominios con ${domainChoice}:`, result);
+    
+	});
+});
